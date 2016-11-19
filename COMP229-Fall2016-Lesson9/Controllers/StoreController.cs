3897b64107da60d91682da01dd2c1c909f36f081
sarinal <sarina-luu@hotmail.com>
2016-11-18 22:31:18 -0500

    Part 2

---
+++ COMP229-Fall2016-Lesson9/Controllers/StoreController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using COMP229_Fall2016_Lesson9.Models;
+
+namespace COMP229_Fall2016_Lesson9.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre("Disco"),
+                new Genre("Jazz"),
+                new Genre("Rock")
+            };
+
+            return View(genres);
+        }
+
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string genre)
+        {
+            if(genre == String.Empty)
+            {
+                genre = "Disco";
+            }
+
+            Genre genreModel = new Genre(genre);
+
+            return View(genreModel);
+        }
+        // GET: Store/Details/5
+        public ActionResult Details(int? id = 1)
+        {
+            Album album = new Album("Album " + id);
+
+            return View(album);
+        }
+        
+    }
+}
